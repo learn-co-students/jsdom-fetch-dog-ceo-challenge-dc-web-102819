77d35303f6290885d28c926ef95ecaf7f6862e5a
github username <github email address>
2019-12-11 15:44:21 -0500

    changes added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 console.log('%c HI', 'color: firebrick')
+
+
+// Challenge 1 
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    fetchdogs() 
+     
+  
+})
+
+function fetchdogs() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(dogArray => {
+        console.log(dogArray)
+        for (let i = 0 ; i < dogArray.message.length ; i++) {
+            makeDog(dogArray.message[i])
+        }
+    })  
+}
+
+
+function makeDog(dog) {
+    let dogcontainer = document.querySelector("#dog-image-container")
+    let dogdiv = document.createElement("div")
+    dogcontainer.appendChild(dogdiv) 
+    let dogimage = document.createElement("img") 
+    dogimage.src = dog
+    dogdiv.appendChild(dogimage) 
+} 
+
+
+// Challenge 2 
+
