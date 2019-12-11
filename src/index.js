68f61871b71ddeb172c4e3791abdef3f14a01871
github username <github email address>
2019-12-11 16:51:25 -0500

    finished challenge2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded",function(){
+    fetchPictures()
+    fetchBreed()
+})
+
+function renderDog(imgUrl){
+    let container = document.getElementById("dog-image-container")
+    let divide = document.createElement('div')
+    container.appendChild(divide)
+    let dogImage = document.createElement("img")
+    dogImage.src = imgUrl
+   
+    divide.appendChild(dogImage)
+}
+
+function listBreed(breed){
+    let ulEl = document.getElementById("dog-breeds")
+    let liEl = document.createElement("li")
+    liEl.innerText = breed
+    ulEl.appendChild(liEl)
+}
+
+function fetchPictures(){
+    fetch(imgUrl)
+    .then(respose => respose.json())
+    .then(dogImages => {
+        dogImages.message.forEach((imgUrl) => {
+            renderDog(imgUrl)
+            
+        })
+    })
+    
+}
+
+function fetchBreed(){
+
+    fetch(breedUrl)
+    .then(respose => respose.json())
+    .then (dogBreed => {
+        
+        Object.keys(dogBreed.message).forEach((breed) => {
+            listBreed(breed)
+        })
+    })
+}
+
+
+
+// function fetchImage(){
+//     fetch(imgUrl)
+//     .then(response => response.json())
+//     .then(dogImage => {
+//         dogImage.forEach((dog) => {
+//         renderDog(dog)
+//         })
+//     })
+// }
+
